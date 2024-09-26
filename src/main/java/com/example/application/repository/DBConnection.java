ba6fe64c99874d2fbfae23886bf2567bba73b32e
sophiasussa <sophiasussa@outlook.com>
2024-09-26 10:48:25 -0300

    first commit

---
+++ src/main/java/com/example/application/repository/DBConnection.java
@@ -0,0 +1,37 @@
+package com.example.application.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static DBConnection instance = null;
+    private Connection conn;
+
+    private final String url = "jdbc:mysql://localhost:3306/strutura";
+    private final String usuario = "root";
+    private final String senha = "H123asd@";
+
+    private DBConnection() throws SQLException{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, usuario, senha);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println("Erro ao conectar ao banco de dados: " + ex.getMessage());
+            throw new SQLException("Falha ao conectar ao banco de dados", ex);
+        }
+    }
+
+    public Connection getConnection(){
+        return conn;
+    }
+
+    public static DBConnection getInstance() throws SQLException{
+        if(instance == null){
+            instance = new DBConnection();
+        } else if (instance.getConnection().isClosed()){
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+}
--- src/main/java/com/example/application/views/cliente/ClienteView.java
+++ src/main/java/com/example/application/views/cliente/ClienteView.java
@@ -2,12 +2,20 @@ package com.example.application.views.cliente;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.button.ButtonVariant;
 
 @PageTitle("Cliente")
 @Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 1)
@@ -24,8 +32,62 @@ public class ClienteView extends Composite<VerticalLayout> {
     }
 
     private void setTabSheetSampleData(TabSheet tabSheet) {
-        tabSheet.add("Dashboard", new Div(new Text("This is the Dashboard tab content")));
-        tabSheet.add("Payment", new Div(new Text("This is the Payment tab content")));
-        tabSheet.add("Shipping", new Div(new Text("This is the Shipping tab content")));
+        Div clientesContent = createClientesContent();
+        tabSheet.add("Clientes", clientesContent);
+
+        Div addClientesContent = createAddClientesContent();
+        tabSheet.add("Adicionar Cliente", addClientesContent);
+    }
+
+    private Div createClientesContent(){
+        Div clientesContentDiv = new Div();
+        VerticalLayout layout = new VerticalLayout();
+
+        return clientesContentDiv;
+    }
+
+    private Div createAddClientesContent(){
+        Div addClientesContentDiv = new Div();
+        Div space = new Div();
+        space.setHeight("10px");
+
+        VerticalLayout layout = new VerticalLayout();
+        VerticalLayout layout2 = new VerticalLayout();
+        VerticalLayout layout3 = new VerticalLayout();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField nome = new TextField("Nome");
+        TextField cpf = new TextField("CNPJ/CPF");
+        TextField rg = new TextField("IE/RG");
+        TextField telefone = new TextField("Telefone");
+        TextArea endereco = new TextArea("Endereço");
+
+        nome.addClassName("rounded-text-field");
+        cpf.addClassName("rounded-text-field");
+        rg.addClassName("rounded-text-field");
+        telefone.addClassName("rounded-text-field");
+        endereco.addClassName("rounded-text-field");
+
+        nome.setRequiredIndicatorVisible(true);
+        telefone.setRequiredIndicatorVisible(true);
+        endereco.setWidth("100%");
+        endereco.setHeight("90px");
+        layout2.getStyle().set("border-radius", "15px");
+        layout2.getStyle().set("border", "1px solid #ccc");
+        layout2.getStyle().set("box-shadow", "0 0 2px rgba(0 , 0, 0, 0.2)");
+        layout3.setAlignItems(FlexComponent.Alignment.END);
+        
+        Button saveButton = new Button("Salvar", event -> {
+            Notification.show("Client saved!");
+        });
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveButton.getStyle().set("border-radius", "25px");
+        
+        formLayout2Col.add(nome, cpf, rg, telefone);
+        layout2.add(formLayout2Col, endereco, space);
+        layout3.add(saveButton);
+        layout.add(layout2, layout3);
+        addClientesContentDiv.add(layout);
+
+        return addClientesContentDiv;
     }
 }
