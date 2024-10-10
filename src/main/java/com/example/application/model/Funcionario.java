71c7ac3b7378c7443d7e3ae4d92c5d11426412f3
sophiasussa <sophiasussa@outlook.com>
2024-10-09 23:59:10 -0300

    Implemented 'Funcionario' view front-end

---
+++ src/main/java/com/example/application/model/Funcionario.java
@@ -0,0 +1,78 @@
+package com.example.application.model;
+import java.time.LocalDate;
+
+public class Funcionario {
+    private Long id;
+    private String nome;
+    private Long cpf;
+    private Long rg;
+    private Long telefone;
+    private String endereco;
+    private LocalDate dataAdmissao;
+    private Double salario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getRg() {
+        return rg;
+    }
+
+    public void setRg(Long rg) {
+        this.rg = rg;
+    }
+
+    public Long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public LocalDate getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(LocalDate dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public Double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(Double salario){
+        this.salario = salario;
+    }
+
+}
--- src/main/java/com/example/application/views/funcionario/FuncionarioView.java
+++ src/main/java/com/example/application/views/funcionario/FuncionarioView.java
@@ -2,11 +2,26 @@ package com.example.application.views.funcionario;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.TabSheet;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import com.example.application.model.Funcionario;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.example.application.views.MainLayout;
 
 
@@ -24,8 +39,107 @@ public class FuncionarioView extends Composite<VerticalLayout> {
     }
 
     private void setTabSheetSampleData(TabSheet tabSheet) {
-        tabSheet.add("Dashboard", new Div(new Text("This is the Dashboard tab content")));
-        tabSheet.add("Payment", new Div(new Text("This is the Payment tab content")));
-        tabSheet.add("Shipping", new Div(new Text("This is the Shipping tab content")));
+        Div funcionarioContent = createFuncionariosContent();
+        tabSheet.add("Funcionários", funcionarioContent);
+
+        Div addFuncionariosContent = createAddFuncionariosContent();
+        tabSheet.add("Adicionar Funcionário", addFuncionariosContent);
     }
+
+    //This method creates the content for the "Funcionários" tab, which consists of a form to see all the employees and search for a specific employee
+    private Div createFuncionariosContent(){
+        Div funcionarioContentDiv = new Div();
+        Div space = new Div();
+        space.setHeight("15px");
+
+        VerticalLayout layout = new VerticalLayout();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        TextField textField = new TextField("Pesquisar");
+        Button buttonPrimary = new Button();
+        Grid<Funcionario> minimalistGrid = new Grid(Funcionario.class, false);
+
+        //For a better interface
+        textField.setPlaceholder("Nome, CNPJ/CPF ou IE/RG");
+        textField.setWidth("250px");
+        layoutRow.setWidthFull();
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setAlignItems(Alignment.END);
+        layoutRow.setJustifyContentMode(JustifyContentMode.END);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("70px");
+        buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        buttonPrimary.setIcon(VaadinIcon.SEARCH.create());
+        buttonPrimary.getStyle().set("border-radius", "50%");
+        textField.addClassName("rounded-text-field");
+        layoutRow.add(textField, buttonPrimary);
+        minimalistGrid.setAllRowsVisible(true);
+
+        buttonPrimary.addClickListener(event -> {
+            Notification.show("Search for: " + textField.getValue());
+        });
+
+        minimalistGrid.addColumn(Funcionario::getNome).setHeader("Nome");
+        minimalistGrid.addColumn(Funcionario::getCpf).setHeader("CNPJ/CPF");
+        minimalistGrid.addColumn(Funcionario::getRg).setHeader("IE/RG");
+        minimalistGrid.addColumn(Funcionario::getTelefone).setHeader("Telefone");
+        minimalistGrid.addColumn(Funcionario::getEndereco).setHeader("Endereço");
+        minimalistGrid.addColumn(Funcionario::getDataAdmissao).setHeader("Admissão");
+        minimalistGrid.addColumn(Funcionario::getSalario).setHeader("Salário");
+
+        layout.add(layoutRow, space, minimalistGrid);
+        funcionarioContentDiv.add(layout);
+
+        return funcionarioContentDiv;
+    }
+
+   //This method creates the content for the "Adicionar Funcionário" tab, which consists of a form to add a new employee.
+    private Div createAddFuncionariosContent(){
+        Div addFuncionariosContentDiv = new Div();
+        Div space = new Div();
+        space.setHeight("10px");
+
+        VerticalLayout layout = new VerticalLayout();
+        VerticalLayout layout2 = new VerticalLayout();
+        VerticalLayout layout3 = new VerticalLayout();
+        FormLayout formLayout2Col = new FormLayout();
+        TextField nome = new TextField("Nome");
+        TextField cpf = new TextField("CNPJ/CPF");
+        TextField rg = new TextField("IE/RG");
+        TextField telefone = new TextField("Telefone");
+        TextArea endereco = new TextArea("Endereço");
+        TextField salario = new TextField("Salário");
+        DatePicker admissao = new DatePicker("Data Admissão");
+
+        Button saveButton = new Button("Salvar", event -> {
+            Notification.show("Client saved!");
+        });
+
+        //For a better interface
+        nome.addClassName("rounded-text-field");
+        cpf.addClassName("rounded-text-field");
+        rg.addClassName("rounded-text-field");
+        telefone.addClassName("rounded-text-field");
+        endereco.addClassName("rounded-text-field");
+        salario.addClassName("rounded-text-field");
+        admissao.addClassName("rounded-text-field");
+        nome.setRequiredIndicatorVisible(true);
+        telefone.setRequiredIndicatorVisible(true);
+        endereco.setWidth("100%");
+        endereco.setHeight("90px");
+        layout2.getStyle().set("border-radius", "15px");
+        layout2.getStyle().set("border", "1px solid #ccc");
+        layout2.getStyle().set("box-shadow", "0 0 2px rgba(0 , 0, 0, 0.2)");
+        layout3.setAlignItems(FlexComponent.Alignment.END);
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveButton.getStyle().set("border-radius", "25px");
+        
+        formLayout2Col.add(nome, cpf, rg, telefone, admissao, salario);
+        layout2.add(formLayout2Col, endereco, space);
+        layout3.add(saveButton);
+        layout.add(layout2, layout3);
+        addFuncionariosContentDiv.add(layout);
+
+        return addFuncionariosContentDiv;
+    }
+
 }
